d9bace2ed7672e078a12628e103a8783b7da8812
kongxuanzhi <875248646@qq.com>
2015-11-13 00:05:15 +0800

    Rejected commit: Default

--- myapp2/src/com/example/myapp2/MyActivity.java
+++ myapp2/src/com/example/myapp2/MyActivity.java
@@ -3,9 +3,12 @@ package com.example.myapp2;
 import android.app.Activity;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
 import com.example.myapp2.model.talkRecord;
 
 import java.util.ArrayList;
@@ -28,15 +31,50 @@ public class MyActivity extends Activity {
 
         //填充list得到数据  从Sqlite数据库中得到数据
         getData();
-        listView.setAdapter(new ListViewAdapter(this,talkRecords));
+        listView.setAdapter(new ListViewAdapter(this, talkRecords));
 
-//        //设置listView 点击事件
-//        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-//            @Override
-//            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-//
-//            }
-//        });
+        //设置listView 点击事件
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                talkRecord getObject = talkRecords.get(i);   //通过position获取所点击的对象
+                int Id = getObject.getID(); //获取信息id
+                String Content = getObject.getContent();
+                String Status = getObject.getStatus();
+
+                //Toast显示测试
+                Toast.makeText(MyActivity.this, "信息:" + Content, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        //长按菜单显示
+        listView.setOnCreateContextMenuListener(new View.OnCreateContextMenuListener() {
+            public void onCreateContextMenu(ContextMenu conMenu, View view, ContextMenu.ContextMenuInfo info) {
+
+                conMenu.setHeaderTitle("菜单");
+                conMenu.add(0, 0, 0, "条目一");
+                conMenu.add(0, 1, 1, "条目二");
+                conMenu.add(0, 2, 2, "条目三");
+            }
+        });
+    }
+
+    //长按菜单处理函数
+    public boolean onContextItemSelected(MenuItem aItem) {
+        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo)aItem.getMenuInfo();
+        switch (aItem.getItemId()) {
+            case 0:
+                Toast.makeText(MyActivity.this, "你点击了条目一",Toast.LENGTH_SHORT).show();
+                return true;
+            case 1:
+                Toast.makeText(MyActivity.this, "你点击了条目二",Toast.LENGTH_SHORT).show();
+
+                return true;
+            case 2:
+                Toast.makeText(MyActivity.this, "你点击了条目三",Toast.LENGTH_SHORT).show();
+                return true;
+        }
+        return false;
     }
 
     public void getData() {
